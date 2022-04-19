02819fd63ffb1d05af8f2227b7999e224ba8cf5e
mgoldm <smirnovnickolay00@mail.com>
2022-04-19 11:15:51 +0300

    screencast part 9

--- app/javascript/packs/answer.js
+++ app/javascript/packs/answer.js
@@ -1,9 +1,22 @@
-$(document).on('turbolinks:load', function(){
-    $('.answers').on('click', '.edit-answer-link', function(e) {
+$(document).on('turbolinks:load', function () {
+    $('.answers').on('click', '.edit-answer-link', function (e) {
         e.preventDefault();
         $(this).hide();
         let answerId = $(this).data('answerId');
         console.log(answerId);
         $('form#edit-answer-' + answerId).removeClass('hidden');
     })
+
+    $('form.new-answer').on('ajax:success', function (e) {
+        let answer = e.detail[0];
+
+        $('.answers').append('<p>' + answer.title + '</p>');
+    })
+        .on('ajax:error', function (e) {
+            let errors = e.detail[0];
+
+            $.each(errors, function (index, value) {
+                $('.answer-errors').append('<p>' + value + '</p>')
+            })
+        })
 });
