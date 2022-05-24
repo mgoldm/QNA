576fbe4c7bd1fecaefd70e7003bc52b74a9e24b8
mgoldm <smirnovnickolay00@mail.com>
2022-05-24 14:44:17 +0300

    fix

--- app/services/daily_digest_service.rb
+++ app/services/daily_digest_service.rb
@@ -2,7 +2,7 @@
 
 class DailyDigestService
   def send_digest
-    questions= Question.check_date
+    questions = Question.check_date
     User.find_each(batch_size: 500) do |user|
       DailyDigestMailer.digest(user, questions).deliver_later if questions.present?
     end
